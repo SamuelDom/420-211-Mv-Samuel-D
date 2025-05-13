267bd9e29c47da1fe28dd0225d1064f75c23414f
SamuelDom <Samueldomesca@outlook.com>
2025-05-12 21:00:02 -0400

    Fourth push - css + footer et navbar + Home update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './Footer.css'
+ 
 function Footer(){
     return (
+        <footer>
         <h4>Website designed and coded by Samuel Richard Domesca</h4>
-    )
+        </footer>
+    );
 }
-
+ 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import './Home.css'
-
+import Sakura from '../Assets/Sakura.mp4'
+ 
 function Home(){
     return(
         <>
-       <div className='home'>
+        <div className='home'>
         <div className='overlay'></div>
-        {/*rentrer un lien vidéo */ }
+        <video src={Sakura} autoPlay loop muted />
         <div className='text'>
             <h1>Welcome to my website!</h1>
+             <form /*onSubmit={}*/ className="form">
+            <input name="user" type="text" placeholder="Username" />
+            <input name="email" type="email" placeholder="user@email.com" />
+            <button>S'inscrire</button>
+            </form>
         </div>
-       </div>
+        </div>
         </>
     )
-
 }
-
+ 
 export default Home
